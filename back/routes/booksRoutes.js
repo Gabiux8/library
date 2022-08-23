0c2bd074a672684b7eaff80fade97b0bd926f19b
Gabiux8 <gabiuxx6661@gmail.com>
2022-08-23 13:24:25 +0300

    library

---
+++ back/routes/booksRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const {
+  getAllBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/booksController");
+
+const router = express.Router();
+
+router.route("/").get(getAllBooks).post(createBook);
+router.route("/delete/:id").get(deleteBook);
+router.route("/update/:id").patch(updateBook);
+
+module.exports = router;
